fb85a73d2155d457f5afd40a77dfceb36ae73fb3
dolphinscathy <liuxiyuan95@gmail.com>
2018-03-04 20:19:29 +0800

    first commit

---
+++ get_positive_features.m
@@ -0,0 +1,16 @@
+function features_pos = get_positive_features(train_path_pos, feature_params)
+
+image_files = dir( fullfile( train_path_pos, '*.jpg') ); %Caltech Faces stored as .jpg
+num_images = length(image_files);
+D = (feature_params.template_size / feature_params.hog_cell_size)^2 * 31;
+
+% placeholder to be deleted
+features_pos = rand(num_images, D);
+
+for i = 1:num_images
+    filename = [train_path_pos '/' image_files(i).name];
+    img = imread(filename);
+    hog = vl_hog(single(img), feature_params.hog_cell_size, 'verbose');
+    hog = reshape(hog, [1, D]);
+    features_pos(i, :) = hog;
+end
