3a77ce9f0b630cf57847629c7393894e0077b028
dolphinscathy <liuxiyuan95@gmail.com>
2018-03-04 20:39:49 +0800

    modify return object

--- get_positive_features.m
+++ get_positive_features.m
@@ -1,10 +1,9 @@
-function features_pos = get_positive_features(train_path_pos, feature_params)
+function [features_pos, num_images] = get_positive_features(train_path_pos, feature_params)
 
 image_files = dir( fullfile( train_path_pos, '*.jpg') ); %Caltech Faces stored as .jpg
 num_images = length(image_files);
 D = (feature_params.template_size / feature_params.hog_cell_size)^2 * 31;
 
-% placeholder to be deleted
 features_pos = rand(num_images, D);
 
 for i = 1:num_images
@@ -13,4 +12,5 @@ for i = 1:num_images
     hog = vl_hog(single(img), feature_params.hog_cell_size, 'verbose');
     hog = reshape(hog, [1, D]);
     features_pos(i, :) = hog;
+end
 end
