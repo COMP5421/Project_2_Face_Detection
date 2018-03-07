fd73933adbabf39c08b11cf8e52c2a1a09ff9d64
Xiyuan Liu <liuxiyuan95@gmail.com>
2018-03-07 23:25:22 +0800

    update average precision up to 0.891

--- run_detector.m
+++ run_detector.m
@@ -10,7 +10,7 @@ image_ids = cell(0, 1);
 cell_size = feature_params.hog_cell_size;
 template_size = feature_params.template_size;
 D = (template_size / cell_size)^2 * 31;
-scales = [1, 0.85, 0.75, 0.6, 0.5, 0.4, 0.25, 0.15, 0.1, 0.07];
+scales = [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05];
 step = 6;
 
 for i = 1:length(test_scenes)
@@ -32,7 +32,7 @@ for i = 1:length(test_scenes)
         			hog = vl_hog(single(window), cell_size);
         			hog = reshape(hog, [1, D]);
         			conf = hog*w+b;
-        			if conf>1
+        			if conf>0.75
                         temp_bboxes = [floor((1+(k-1)*step)/scal), floor((1+(j-1)*step)/scal), floor((1+(k-1)*step+template_size-1)/scal), floor((1+(j-1)*step+template_size-1)/scal)];
         				temp_confidences = conf;
         				temp_image_ids = test_scenes(i).name;
